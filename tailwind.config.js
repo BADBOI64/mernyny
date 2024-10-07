cb6344a9a9dca78e6ffbfa7343d98aac885fd8c6
BADBOI64 <emilsonne8@gmail.com>
2024-10-07 03:15:22 +0200

    23

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,5 +13,17 @@ module.exports = {
       },
     },
   },
-  plugins: [require("tailwind-scrollbar-hide")],
+  plugins: [
+    function ({ addUtilities }) {
+      addUtilities({
+        ".scrollbar-hide": {
+          "-ms-overflow-style": "none",
+          "scrollbar-width": "none",
+          "&::-webkit-scrollbar": {
+            display: "none",
+          },
+        },
+      });
+    },
+  ],
 };
